3fe9972ac190baa1af34ebafba0ee9e672a3de0f
apriloysun <apriloysun@tencent.com>
2023-10-15 23:24:57 +0800

    feat: quiz2完成

--- exercises/hashmaps/hashmaps2.rs
+++ exercises/hashmaps/hashmaps2.rs
@@ -14,7 +14,6 @@
 // Execute `rustlings hint hashmaps2` or use the `hint` watch subcommand for a
 // hint.
 
-// I AM NOT DONE
 
 use std::collections::HashMap;
 
@@ -40,6 +39,14 @@ fn fruit_basket(basket: &mut HashMap<Fruit, u32>) {
         // TODO: Insert new fruits if they are not already present in the
         // basket. Note that you are not allowed to put any type of fruit that's
         // already present!
+        // if !basket.contains_key(&fruit) {
+        //     match fruit {
+        //         Fruit::Banana => basket.insert(Fruit::Banana, 2),
+        //         Fruit::Pineapple => basket.insert(Fruit::Pineapple, 2),
+        //         _ => None
+        //     };
+        // }
+        basket.entry(fruit).or_insert(2);
     }
 }
 
